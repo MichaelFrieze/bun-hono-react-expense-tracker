afcb1730f97bfd044c14a4a19d184e7058fef2f3
Michael Frieze <michaelfrieze@icloud.com>
2024-05-04 23:30:50 -0400

    add fake expenses data

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,9 +1,24 @@
 import { Hono } from 'hono';
 
+type Expense = {
+  id: number;
+  title: string;
+  amount: number;
+};
+
+const fakeExpenses: Expense[] = [
+  // make some expenses for me
+  { id: 1, title: 'Groceries', amount: 50 },
+  { id: 2, title: 'Utilities', amount: 100 },
+  { id: 3, title: 'Rent', amount: 1000 },
+];
+
 export const expensesRoute = new Hono()
   .get('/', (c) => {
-    return c.json({ expenses: [] });
+    return c.json({ expenses: fakeExpenses });
   })
-  .post('/', (c) => {
-    return c.json({});
+  .post('/', async (c) => {
+    const expense = await c.req.json();
+    console.log({ expense });
+    return c.json(expense);
   });
